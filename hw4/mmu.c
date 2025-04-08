42e55e9640c4b7bbc3c4843779143d9cf969fb6a
coopergolemme <coopergolemme@gmail.com>
2025-04-08 17:00:19 -0400

    finish part 2

--- hw4/mmu.c
+++ hw4/mmu.c
@@ -2,10 +2,8 @@
 #include <math.h>
 #include <assert.h>
 
-
-pte_t* ptbr = NULL; // Page Table Base Register
+pte_t *ptbr = NULL; // Page Table Base Register
                     // points to the start of the current page table
-
 
 /* get_vpn
  *   (optional helper function)
@@ -15,11 +13,14 @@ pte_t* ptbr = NULL; // Page Table Base Register
  *
  * returns the VPN from the addr
  */
-vpn_t get_vpn(virt_addr_t addr) {
-  // TODO
-  return 0;
+vpn_t get_vpn(virt_addr_t addr)
+{
+  // Number of bits for the offset depends on the page size
+  int offset_bits = log2(PAGE_SIZE);
+  // Shift over to get higher order bits which correspond to vpn
+  vpn_t vpn = addr >> offset_bits;
+  return vpn;
 }
-
 
 /* get_offset
  *   (optional helper function)
@@ -29,11 +30,15 @@ vpn_t get_vpn(virt_addr_t addr) {
  *
  * returns the page offset from the addr
  */
-offset_t get_offset(virt_addr_t addr) {
-  // TODO
-  return 0;
+offset_t get_offset(virt_addr_t addr)
+{
+  int offset_bits = log2(PAGE_SIZE);
+  // make mask of all 1's offset in width
+  int mask = (1 << offset_bits) - 1;
+  // and to get the lower order bits for offset
+  offset_t offset = addr & mask;
+  return offset;
 }
-
 
 /* translate_addr
  *   translates a virtual memory address to a physical memory address
@@ -45,7 +50,26 @@ offset_t get_offset(virt_addr_t addr) {
  * returns the phsyical memory address where the virtual addr is stored
  *   or PAGE_FAULT if the virtual page is not currently in memory
  */
-phys_addr_t translate_addr(virt_addr_t virt_addr, bool_t write) {
-  // TODO
-  return 0;
+phys_addr_t translate_addr(virt_addr_t virt_addr, bool_t write)
+{
+  // stop compiler from yelling
+  // TODO: encorperate this
+  (void)write;
+
+  vpn_t vpn = get_vpn(virt_addr);
+  offset_t offset = get_offset(virt_addr);
+
+  // Locate the page table entry using the vpn
+  pte_t *pte = &ptbr[vpn];
+
+  // if page is in memory
+  if (!pte->present)
+  {
+    return PAGE_FAULT;
+  }
+
+  int offset_bits = log2(PAGE_SIZE);
+  phys_addr_t phys_addr = (pte->pfn << offset_bits) | offset;
+
+  return phys_addr;
 }
--- hw4/page_fault_handler.c
+++ hw4/page_fault_handler.c
@@ -4,24 +4,39 @@
 #include <string.h>
 #include <assert.h>
 #include <stdlib.h>
+#include <stdio.h>
 
+#define NUM_FRAMES NUM_PAGES
+
+typedef struct
+{
+  bool_t is_used; // TRUE if the frame is in use, FALSE otherwise
+  vpn_t vpn;      // Virtual Page Number mapped to this frame
+  pid_t owner;    // Process ID of the owner of this frame
+} frame_t;
+
+frame_t *frame_table;
 
 /* Process Control Block (PCB)
  *   stores data relevant to a single process
  *   (all we really care about for this simulation is the page table)
  */
-typedef struct {
-  pte_t* page_table; // base address of this proc's page table
+typedef struct
+{
+  pte_t *page_table; // base address of this proc's page table
 } pcb_t;
 
 pcb_t pcb[PID_MAX]; // we need a PCB for each proc
 
+void init_frame_table();
+void init_page_table();
 
 /* init_vmm
  *   called exactly once when the simulation starts
  *   so you can do any initialization you need to do
  */
-void init_vmm(void) {
+void init_vmm(void)
+{
   // This implementation provides a static pre-filled page table,
   // with the first 8 pages (VPN 0 - 7) present and mapped to PFNs.
   // This allows testing address translations alone (mmu.c),
@@ -30,32 +45,62 @@ void init_vmm(void) {
 
   ptbr = malloc(NUM_PAGES * sizeof(pte_t));
   assert(NULL != ptbr); // chech for Out of Memory error
-  memset(ptbr, 0, NUM_PAGES * sizeof(pte_t));
+  init_page_table();
 
-  pfn_t pfn[8] = { 0x00, 0x01, 0x03, 0x02, 0x04, 0x07, 0x06, 0x05 };
-  for (vpn_t vpn = 0; vpn < 8; ++vpn) {
-    ptbr[vpn].pfn = pfn[vpn];
-    ptbr[vpn].present = TRUE;
-  }
-
-  // TODO: replace the above code with initializations that you need to do
+  frame_table = malloc(NUM_FRAMES * sizeof(frame_t));
+  assert(NULL != frame_table);
+  init_frame_table();
 }
 
+void init_frame_table()
+{
+  // Initialize each frame in the frame table
+  for (pfn_t pfn = 0; pfn < NUM_FRAMES; pfn++)
+  {
+    frame_table[pfn].is_used = FALSE; // Frame is not in use
+    frame_table[pfn].vpn = 0;         // Default VPN (placeholder)
+    frame_table[pfn].owner = -1;      // No owner (invalid PID)
+  }
 
+  printf("Frame table initialized with %d frames.\n", NUM_FRAMES);
+}
+
+void init_page_table()
+{
+  for (pid_t pid = 0; pid < PID_MAX; pid++)
+  {
+    pcb[pid].page_table = malloc(NUM_PAGES * sizeof(pte_t));
+    assert(NULL != pcb[pid].page_table); // Check for out-of-memory errors
+
+    // Initialize each page table entry
+    for (vpn_t vpn = 0; vpn < NUM_PAGES; vpn++)
+    {
+      pcb[pid].page_table[vpn].pfn = 0;
+      pcb[pid].page_table[vpn].present = FALSE;
+      pcb[pid].page_table[vpn].reference = FALSE;
+      pcb[pid].page_table[vpn].dirty = 0;
+    }
+  }
+
+  printf("Page tables initialized for %d processes.\n", PID_MAX);
+}
 /* cleanup_vmm
  *   called exactly once just before the simulation finished
  *   so you can do any cleanup you need to do
  */
-void cleanup_vmm(void) {
+void cleanup_vmm(void)
+{
   // Cleanup corresponding to the provied init implementation above,
   // but this will also need to be replaced when the init is changed
   // to its final verion.
   free(ptbr);
   ptbr = NULL;
 
+  free(frame_table);
+  frame_table = NULL;
+
   // TODO: replace the above code with cleanup that you need to do
 }
-
 
 /* context_switch
  *   called when a context switch happens, so you can update state as needed
@@ -64,10 +109,10 @@ void cleanup_vmm(void) {
  *
  * (N.B. postcondition omitted so you can figure out what state needs to be updated)
  */
-void context_switch(pid_t new_proc) {
-  // TODO
+void context_switch(pid_t new_proc)
+{
+  ptbr = pcb[new_proc].page_table;
 }
-
 
 /* find_free_frame
  *   helper function to search for a free frame of physical memory
@@ -79,11 +124,21 @@ void context_switch(pid_t new_proc) {
  * postcondition: the returned frame does NOT contain a page
  *   (i.e., is_used is FALSE)
  */
-pfn_t find_free_frame(void) {
-  // TODO
+pfn_t find_free_frame(void)
+{
+  // Iterate over all physical frames
+  for (pfn_t pfn = 0; pfn < NUM_FRAMES; pfn++)
+  {
+    // Check if the frame is free (not in use)
+    if (!frame_table[pfn].is_used)
+    {
+      return pfn; // Return the PFN of the free frame
+    }
+  }
+
+  // If no free frame is found, return -1
   return -1;
 }
-
 
 /* find_victim_page
  *   implements the page replacement policy by choosing a victim page
@@ -95,12 +150,12 @@ pfn_t find_free_frame(void) {
  *
  * postcondition: the returned frame contains a page (i.e., is_used is TRUE)
  */
-pfn_t find_victim_page(void) {
+pfn_t find_victim_page(void)
+{
   // TODO
   // Clock Sweep
   return 0;
 }
-
 
 /* page_fault_handler
  *   this is called when a page fault occurs
@@ -121,6 +176,33 @@ pfn_t find_victim_page(void) {
  *   but postconditions for those have not been documented so you can
  *   figure out what you need to do on your own.)
  */
-void page_fault_handler(pid_t faulting_proc, virt_addr_t faulting_addr) {
-  // TODO
+void page_fault_handler(pid_t faulting_proc, virt_addr_t faulting_addr)
+{
+  // printf("Page fault handler\n");
+  pfn_t free_frame = find_free_frame();
+  // if (free_frame == -1)
+  // {
+  //   printf("No free frame found. Need to evict a page.\n");
+  //   free_frame = find_victim_page();
+  // }
+  // else
+  // {
+  //   // printf("Free frame found: PFN %d\n", free_frame);
+  // }
+
+  // need to handle no free frame found
+
+  // load faulting page into free frame
+  vpn_t faulting_vpn = get_vpn(faulting_addr);
+  load_page_from_disk(faulting_proc, faulting_vpn, free_frame);
+
+  // Update the page table of the faulting process
+  pcb[faulting_proc].page_table[faulting_vpn].pfn = free_frame;
+  pcb[faulting_proc].page_table[faulting_vpn].present = TRUE;
+  pcb[faulting_proc].page_table[faulting_vpn].dirty = FALSE; // Reset dirty bit
+
+  // Update the frame tableb
+  frame_table[free_frame].is_used = TRUE;
+  frame_table[free_frame].vpn = faulting_vpn;
+  frame_table[free_frame].owner = faulting_proc;
 }

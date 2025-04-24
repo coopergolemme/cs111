3c02ee0971bc8858a1fb526ec5c1885ad6fd84bc
coopergolemme <coopergolemme@gmail.com>
2025-04-24 18:49:04 -0400

    Start hw5 and finish hw3 and 4

---
+++ hw5/jumbo_file_system.c
@@ -0,0 +1,171 @@
+#include "jumbo_file_system.h"
+
+// C does not have a bool type, so I created one that you can use
+typedef char bool_t;
+#define TRUE 1
+#define FALSE 0
+
+
+static block_num_t current_dir;
+
+
+// optional helper function you can implement to tell you if a block is a dir node or an inode
+static bool_t is_dir(block_num_t block_num) {
+  return TRUE;
+}
+
+
+/* jfs_mount
+ *   prepares the DISK file on the _real_ file system to have file system
+ *   blocks read and written to it.  The application _must_ call this function
+ *   exactly once before calling any other jfs_* functions.  If your code
+ *   requires any additional one-time initialization before any other jfs_*
+ *   functions are called, you can add it here.
+ * filename - the name of the DISK file on the _real_ file system
+ * returns 0 on success or -1 on error; errors should only occur due to
+ *   errors in the underlying disk syscalls.
+ */
+int jfs_mount(const char* filename) {
+  int ret = bfs_mount(filename);
+  current_dir = 1;
+  return ret;
+}
+
+
+/* jfs_mkdir
+ *   creates a new subdirectory in the current directory
+ * directory_name - name of the new subdirectory
+ * returns 0 on success or one of the following error codes on failure:
+ *   E_EXISTS, E_MAX_NAME_LENGTH, E_MAX_DIR_ENTRIES, E_DISK_FULL
+ */
+int jfs_mkdir(const char* directory_name) {
+  return E_UNKNOWN;
+}
+
+
+/* jfs_chdir
+ *   changes the current directory to the specified subdirectory, or changes
+ *   the current directory to the root directory if the directory_name is NULL
+ * directory_name - name of the subdirectory to make the current
+ *   directory; if directory_name is NULL then the current directory
+ *   should be made the root directory instead
+ * returns 0 on success or one of the following error codes on failure:
+ *   E_NOT_EXISTS, E_NOT_DIR
+ */
+int jfs_chdir(const char* directory_name) {
+  return E_UNKNOWN;
+}
+
+
+/* jfs_ls
+ *   finds the names of all the files and directories in the current directory
+ *   and writes the directory names to the directories argument and the file
+ *   names to the files argument
+ * directories - array of strings; the function will set the strings in the
+ *   array, followed by a NULL pointer after the last valid string; the strings
+ *   should be malloced and the caller will free them
+ * file - array of strings; the function will set the strings in the
+ *   array, followed by a NULL pointer after the last valid string; the strings
+ *   should be malloced and the caller will free them
+ * returns 0 on success or one of the following error codes on failure:
+ *   (this function should always succeed)
+ */
+int jfs_ls(char* directories[MAX_DIR_ENTRIES+1], char* files[MAX_DIR_ENTRIES+1]) {
+  return E_UNKNOWN;
+}
+
+
+/* jfs_rmdir
+ *   removes the specified subdirectory of the current directory
+ * directory_name - name of the subdirectory to remove
+ * returns 0 on success or one of the following error codes on failure:
+ *   E_NOT_EXISTS, E_NOT_DIR, E_NOT_EMPTY
+ */
+int jfs_rmdir(const char* directory_name) {
+  return E_UNKNOWN;
+}
+
+
+/* jfs_creat
+ *   creates a new, empty file with the specified name
+ * file_name - name to give the new file
+ * returns 0 on success or one of the following error codes on failure:
+ *   E_EXISTS, E_MAX_NAME_LENGTH, E_MAX_DIR_ENTRIES, E_DISK_FULL
+ */
+int jfs_creat(const char* file_name) {
+  return E_UNKNOWN;
+}
+
+
+/* jfs_remove
+ *   deletes the specified file and all its data (note that this cannot delete
+ *   directories; use rmdir instead to remove directories)
+ * file_name - name of the file to remove
+ * returns 0 on success or one of the following error codes on failure:
+ *   E_NOT_EXISTS, E_IS_DIR
+ */
+int jfs_remove(const char* file_name) {
+  return E_UNKNOWN;
+}
+
+
+/* jfs_stat
+ *   returns the file or directory stats (see struct stat for details)
+ * name - name of the file or directory to inspect
+ * buf  - pointer to a struct stat (already allocated by the caller) where the
+ *   stats will be written
+ * returns 0 on success or one of the following error codes on failure:
+ *   E_NOT_EXISTS
+ */
+int jfs_stat(const char* name, struct stats* buf) {
+  return E_UNKNOWN;
+}
+
+
+/* jfs_write
+ *   appends the data in the buffer to the end of the specified file
+ * file_name - name of the file to append data to
+ * buf - buffer containing the data to be written (note that the data could be
+ *   binary, not text, and even if it is text should not be assumed to be null
+ *   terminated)
+ * count - number of bytes in buf (write exactly this many)
+ * returns 0 on success or one of the following error codes on failure:
+ *   E_NOT_EXISTS, E_IS_DIR, E_MAX_FILE_SIZE, E_DISK_FULL
+ */
+int jfs_write(const char* file_name, const void* buf, unsigned short count) {
+  return E_UNKNOWN;
+}
+
+
+/* jfs_read
+ *   reads the specified file and copies its contents into the buffer, up to a
+ *   maximum of *ptr_count bytes copied (but obviously no more than the file
+ *   size, either)
+ * file_name - name of the file to read
+ * buf - buffer where the file data should be written
+ * ptr_count - pointer to a count variable (allocated by the caller) that
+ *   contains the size of buf when it's passed in, and will be modified to
+ *   contain the number of bytes actually written to buf (e.g., if the file is
+ *   smaller than the buffer) if this function is successful
+ * returns 0 on success or one of the following error codes on failure:
+ *   E_NOT_EXISTS, E_IS_DIR
+ */
+int jfs_read(const char* file_name, void* buf, unsigned short* ptr_count) {
+  return E_UNKNOWN;
+}
+
+
+/* jfs_unmount
+ *   makes the file system no longer accessible (unless it is mounted again).
+ *   This should be called exactly once after all other jfs_* operations are
+ *   complete; it is invalid to call any other jfs_* function (except
+ *   jfs_mount) after this function complete.  Basically, this closes the DISK
+ *   file on the _real_ file system.  If your code requires any clean up after
+ *   all other jfs_* functions are done, you may add it here.
+ * returns 0 on success or -1 on error; errors should only occur due to
+ *   errors in the underlying disk syscalls.
+ */
+int jfs_unmount() {
+  int ret = bfs_unmount();
+  return ret;
+}
